fbc93321012014a3be7b48ef3a4edfaa618e1970
sasivenkat143143@outlook.com <sasivenkat143143@outlook.com>
2023-11-02 13:30:19 +0000

    Created lendor_registration_form.dashboard and lendor_registration_form.dashboard.opbal; Edited settings, theme/parameters.yaml and bank_users.user_form

---
+++ client_code/lendor_registration_form/dashboard/__init__.py
@@ -0,0 +1,25 @@
+from ._anvil_designer import dashboardTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+
+class dashboard(dashboardTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("bank_users.user_form")
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("bank_users.main_form")
+
+  def button_3_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.dashboard.opbal")
