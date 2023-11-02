cecd55eaeb1d3c7640354988dc2cee77644c4954
sasivenkat143143@outlook.com <sasivenkat143143@outlook.com>
2023-11-02 17:36:31 +0000

    Edited lendor_registration_form.dashboard, theme/parameters.yaml and lendor_registration_form.dashboard.opbal

--- client_code/lendor_registration_form/dashboard/__init__.py
+++ client_code/lendor_registration_form/dashboard/__init__.py
@@ -26,9 +26,55 @@ class dashboard(dashboardTemplate):
 
   def button_4_click(self, **event_args):
     """This method is called when the button is clicked"""
-    open_form("")
-    pass
+    open_form("lendor_registration_form.avlbal")
+
+  def button_5_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.dashboard.vblr")
+
+  def button_6_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.dashboard.ld")
+
+  def outlined_button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.dashboard.td")
+
+  def outlined_button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.dashboard.vlo")
+
+  def outlined_button_3_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.dashboard.vcl")
+
+  def outlined_button_4_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.dashboard.vler")
+
+  def outlined_button_5_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.dashboard.vlfr")
+
+  def outlined_button_6_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.dashboard.rta")
+
+  def outlined_button_7_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.dashboard.vdp")
 
   def outlined_button_8_click(self, **event_args):
     """This method is called when the button is clicked"""
-    open_form("len")
+    open_form("lendor_registration_form.dashboard.vep")
+
+  def outlined_button_9_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.dashboard.vsn")
+
+  def outlined_button_10_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("lendor_registration_form.dashboard.cp")
+    
+
+  
--- client_code/lendor_registration_form/dashboard/opbal/__init__.py
+++ client_code/lendor_registration_form/dashboard/opbal/__init__.py
@@ -14,8 +14,44 @@ class opbal(opbalTemplate):
 
   def link_1_click(self, **event_args):
     """This method is called when the link is clicked"""
-    pass
+    open_form("lendor_registration_form.avlbal")
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form("lendor_registration_form.dashboard")
+
+  def link_2_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("lendor_registration_form.dashboard.vblr")
+
+  def link_3_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("lendor_registration_form.dashboard.ld")
+
+  def link_4_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("lendor_registration_form.dashboard.vlo")
+
+  def link_5_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("lendor_registration_form.dashboard.td")
+
+  def link_6_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("lendor_registration_form.dashboard.vcl")
+
+  def link_7_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("lendor_registration_form.dashboard.vler")
+
+  def link_8_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("lendor_registration_form.dashboard.vlfr")
+
+  def link_9_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("lendor_registration_form.dashboard.rta")
+
+  def link_10_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("lendor_registration_form.dashboard.vdp")
