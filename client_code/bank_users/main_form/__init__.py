d94c2d3fe29467c1dc6ea184b71cd6fc86a4c106
bejawadaprem@gmail.com <bejawadaprem@gmail.com>
2023-11-02 05:43:11 +0000

    Cloned app

---
+++ client_code/bank_users/main_form/__init__.py
@@ -0,0 +1,63 @@
+from ._anvil_designer import main_formTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+from ..user_form import user_module
+from . import main_form_module
+
+class main_form(main_formTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    
+
+
+  #it is used to login or sign up the user
+  def login_sign_up_main_form_link_click(self, **event_args):
+    anvil.users.login_with_form()
+    current_user = anvil.users.get_user()
+    if current_user:
+      user_email = current_user['email']
+      print(user_email)
+      print(user_email)
+      check_user_already_exist = user_module.check_user_profile(user_email)
+      print(check_user_already_exist)
+      if check_user_already_exist == None:
+        user_module.add_email_and_user_id(user_email)
+        main_form_module.email=user_email
+        main_form_module.flag=True
+        open_form('bank_users.user_form')
+      else:
+        main_form_module.email=user_email
+        main_form_module.flag=False
+        open_form('bank_users.user_form')
+    
+
+
+
+
+          
+  #form maping no need to change 
+  def about_main_form_link_click(self, **event_args):
+    open_form('bank_users.main_form.about_main_form')
+
+  def contact_main_form_link_click(self, **event_args):
+    open_form('bank_users.main_form.contact_main_form')
+
+  def carrer_main_form_link_click(self, **event_args):
+    open_form('bank_users.main_form.Carrer_main_form')
+
+  def location_main_form_link_click(self, **event_args):
+    open_form('bank_users.main_form.location_main_form')
+
+
+
+
+
+
+
---
+++ client_code/bank_users/user_form/user_module.py
@@ -0,0 +1,66 @@
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+
+def generate_user_id():
+    full_table = app_tables.user_profile.search()
+    if full_table:
+        highest_coustmer_id = find_highest_amigos_id()
+        return highest_coustmer_id + 1
+    else:
+        return 1000
+
+def find_highest_amigos_id():
+    table_data = app_tables.user_profile.search()
+    highest_id = 999
+    for row in table_data:
+        coustmer_id = row['coustmer_id']
+        if coustmer_id > highest_id:
+            highest_id = coustmer_id
+    return highest_id
+
+# It creates the ID for only new users
+def add_email_and_user_id(email_id):
+    generated_id = generate_user_id()
+    app_tables.user_profile.add_row(email_user=email_id, coustmer_id=generated_id)
+
+
+# The method checks whether the user exists or not
+def check_user_profile(email_id):
+    user_check = app_tables.user_profile.search(email_user=email_id)
+    if user_check:
+      for user in user_check:
+        if user['email_user'] == email_id:
+          return True
+    else:
+        return False
+
+
+def get_name(email):
+  parts = email.split("@")
+  if len(parts) == 2:
+    split_name = parts[0]
+    return split_name
+
+
+def find_user_id(email):
+  user_true = app_tables.user_profile.search(email_user=email)
+  if user_true:
+    coustmer_id = user_true[0]['coustmer_id']
+    return coustmer_id
+  else:
+    return 0000
+
+def last_check_status(user_id):
+  return False
+
+def registration_engine():
+  pass
+  
+  
