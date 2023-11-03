31eba5375f817c31ad8bb863bc32e0daa494dcfa
maniamarthi@gmail.com <maniamarthi@gmail.com>
2023-11-03 05:01:03 +0000

    Created lendor_registration_form.avlbal, lendor_registration_form.avlbal.RowTemplate3, lendor_registration_form.avlbal.RowTemplate4 and lendor_registration_form.avlbal.RowTemplate2; Edited bank_users.borrower_registration_main_form.application_tracker, lendor_registration_form.dashboard, lendor_registration_form.dashboard.cp, lendor_registration_form.dashboard.ld and 12 more

---
+++ client_code/lendor_registration_form/avlbal/__init__.py
@@ -0,0 +1,31 @@
+from ._anvil_designer import avlbalTemplate
+from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+class avlbal(avlbalTemplate):
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+
+      
+        #self.repeating_panel_1.items = app_tables.view_bor_loan_requests.search(self.user_id)
+
+        # Define the user ID for the specific user you want to retrieve the available balance for
+        self.user_id = '489'
+
+        # Fetch the data for the specific user from your table
+        user_request = app_tables.view_bor_loan_requests.get(user_id=self.user_id)
+
+        if user_request:
+            initial_commitment = user_request['initial_commitment']
+            top_up = user_request['top_up']
+            total_loan_disbursed = user_request['total_loan_disbursed']
+            available_balance = int(initial_commitment) + int(top_up) - int(total_loan_disbursed)
+            self.output_lbl.text = f" {available_balance}"
+        else:
+            self.label1.text = f"User {self.user_id} not found or data not available."
+        user_requests = app_tables.view_bor_loan_requests.search(user_id=self.user_id)
+        self.repeating_panel_1.items = user_requests
