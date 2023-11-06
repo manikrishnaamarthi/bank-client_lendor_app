83121125f116e3101da7b6d82f42a5b97936f82d
bejawadaprem@gmail.com <bejawadaprem@gmail.com>
2023-11-06 09:09:31 +0000

    Edited settings, lendo_server and lendor_registration_form.Lender_reg_form_1

--- server_code/lendo_server.py
+++ server_code/lendo_server.py
@@ -6,6 +6,6 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 
-@anvil.server.callable
-def add_lendor_frist_form(name,gender,city,user_id):
-  app_tables.user_profile.add_row(name=name,gender=gender,city=city, coustmer_id=user_id )
+#@anvil.server.callable
+#def add_lendor_frist_form(name,gender,city,user_id):
+  #app_tables.user_profile.add_row(name=name,gender=gender,city=city, coustmer_id=user_id )
