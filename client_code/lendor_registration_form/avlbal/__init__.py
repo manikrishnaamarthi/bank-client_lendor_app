8a43f6715c270229958a784c68e9565607b18a36
maniamarthi@gmail.com <maniamarthi@gmail.com>
2023-11-07 10:26:52 +0000

    Edited lendor_registration_form.avlbal, standard-page.html and lendor_registration_form.dashboard

--- client_code/lendor_registration_form/avlbal/__init__.py
+++ client_code/lendor_registration_form/avlbal/__init__.py
@@ -14,7 +14,7 @@ class avlbal(avlbalTemplate):
         #self.repeating_panel_1.items = app_tables.view_bor_loan_requests.search(self.user_id)
 
         # Define the user ID for the specific user you want to retrieve the available balance for
-        self.user_id = '1000'
+        self.user_id = '4891'
 
         # Fetch the data for the specific user from your table
         user_request = app_tables.view_bor_loan_requests.get(user_id=self.user_id)
@@ -26,7 +26,7 @@ class avlbal(avlbalTemplate):
             available_balance = int(initial_commitment) + int(top_up) - int(total_loan_disbursed)
             self.output_lbl.text = f" {available_balance}"
         else:
-            self.label1.text = f"User {self.user_id} not found or data not available."
+            self.label_1.text = f"User {self.user_id} not found or data not available."
         user_requests = app_tables.view_bor_loan_requests.search(user_id=self.user_id)
         self.repeating_panel_1.items = user_requests
 
